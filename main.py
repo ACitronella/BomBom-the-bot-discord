6c6a905ed9ec14e2d5d8e14b1e27f386366e6205
ACitronella <phuriwat.13042@gmail.com>
2020-12-10 01:58:14 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+class BOMTHEBOT(discord.Client):
+    async def on_message(self, message):
+        if message.author != self.user:
+            print("ur mum") 
+
+
+bot = BOMTHEBOT()
+bot.run(BOT_TOKEN)
+
