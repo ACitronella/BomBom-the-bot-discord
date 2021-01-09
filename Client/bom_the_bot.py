c30e5890fc556a1525545912720c72db30016c56
ACitronella <phuriwat.13042@gmail.com>
2021-01-09 18:49:58 +0700

    boat adjust structure and add chatbot service

---
+++ Client/bom_the_bot.py
@@ -0,0 +1,31 @@
+import settings as ENV
+import discord
+from .Services.chatbot import ChatBotService
+
+class BOMTHEBOT(discord.Client):
+
+    async def on_message(self, message):
+        # boolean
+        is_not_self = message.author != self.user
+        is_right_channel = message.channel.name in ENV.ALLOW_CHANNELS
+        is_prefix_bom = message.content.startswith(ENV.ACIVATE_MESSAGE)
+        if not (is_not_self and is_right_channel and is_prefix_bom):
+            return
+
+        message_bot_got = message.content[len(ENV.ACIVATE_MESSAGE):]
+        if message_bot_got == "exit":
+            await message.channel.send("บาย")
+            await self.close()
+
+        print("bot got a message:", message_bot_got, "from:", message.author.name)
+        from_api = ChatBotService.botnoichitchat(message_bot_got) # call api from botnoi
+        await message.channel.send(from_api)
+        
+
+    async def on_connect(self):
+        all_ch = self.get_all_channels()
+        for channel in all_ch:
+            # print(channel.name)
+            if channel.name in ENV.ALLOW_CHANNELS:
+                await channel.send("hi! im bom")
+
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import settings as ENV
 from Client.bom_the_bot import BOMTHEBOT
+import logging
 
-print("ACIVATE_MESSAGE =", ENV.ACIVATE_MESSAGE)
-print("ALLOW_CHANNELS =",ENV.ALLOW_CHANNELS)
-print("BOT_NOI_TOKEN =", ENV.BOT_NOI_TOKEN)
-print("DISCORD_TOKEN =", ENV.DISCORD_TOKEN)
-print("CHATBOT_STYLE =", ENV.CHATBOT_STYLE)
-print("HEADERS =", ENV.HEADERS)
+# print("ACIVATE_MESSAGE =", ENV.ACIVATE_MESSAGE)
+# print("ALLOW_CHANNELS =",ENV.ALLOW_CHANNELS)
+# print("BOT_NOI_TOKEN =", ENV.BOT_NOI_TOKEN)
+# print("DISCORD_TOKEN =", ENV.DISCORD_TOKEN)
+# print("CHATBOT_STYLE =", ENV.CHATBOT_STYLE)
+# print("HEADERS =", ENV.HEADERS)
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot = BOMTHEBOT()
 bot.run(ENV.DISCORD_TOKEN)
